9743a31846d0a4ef578023d2c31e3d38cfdaf59e
Cristian David Naranjo Orjuela <111905823+cristiandavid0124@users.noreply.github.com>
2024-05-09 22:40:41 -0500

    Revert "juntando landing page con pagina principal de la app"

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Courses from './components/Courses';
-import Reviews from './components/Reviews';
-import Footer from './components/Footer';
-import HeaderPage from './components/HeaderPage';
-import Options from './components/Options';
-import ClothingSelector from './components/ClothingSelect';
-import OutfitgridSave from './components/OutfitgridSave';
-import OutfitGridWithLikes from './components/OutfitGridLikes';
-import ClosetOptions from './components/ClosetOption';
-import ClosetGrid from './components/ClosetGrid';
-import AcountOptions from './components/AcountOptions';
-import UserInfoGrid from './components/UserInfoGrid';
-import ConfigurationsUser from './components/ConfigurationsUser';
+import { Hero, Navbar, Courses, Reviews, Footer } from './components';
+import './App.css'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <Navbar />
-              <Hero />
-              <Courses />
-              <Reviews />
-              <Footer />
-            </div>
-          } />
-          <Route path="/Styling/*" element={
-            <div>
-              <HeaderPage />
-              <Options />
-              <Routes>
-                <Route index element={<div></div>} />
-                <Route path="Swipe ur syle" element={<ClothingSelector />} />
-                <Route path="inspiration" element={<OutfitgridSave />} />
-                <Route path="Ur favorites" element={<OutfitGridWithLikes />} />
-              </Routes>
-            </div>
-          } />
-          <Route path="/schedule" element={<div></div>} />
-          <Route path="/closet/*" element={
-            <div>
-              <HeaderPage />
-              <ClosetOptions />
-              <Routes>
-                <Route path="My Closet" element={<ClosetGrid />} />
-                <Route path="Paper Bin" element={<div>Contenido de My Paper Bin</div>} />
-              </Routes>
-            </div>
-          } />
-          <Route path="/account*" element={
-            <div>
-              <HeaderPage />
-              <AcountOptions />
-              <Routes>
-                <Route path="Account" element={<UserInfoGrid />} />
-                <Route path="Configurations" element={<ConfigurationsUser />} />
-              </Routes>
-            </div>
-          } />
-        </Routes>
-      </Router>
+    <div>
+      <Navbar />
+      <Hero />
+      <Courses />
+      <Reviews />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,60 @@
 import React, { useState } from 'react';
-import { logo, lock, close } from '../assets';
-import { Link } from 'react-router-dom';
+import {logo,lock, close} from '../assets'
 
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false);
 
-    const handleClick = () => setToggle(!toggle);
+    const [toggle,setToggle]=useState(false)
+    const handleClick = ()=> setToggle(!toggle)
 
-    return (
-        <div className='w-full h-[80px] bg-white border-b'>
-            <div className='md:max-w-[1480px] max-w-[600px] m-auto w-full h-full flex justify-between items-center md:px-0 px-4'>
-                <img src={logo} className="h-[200px]" />
-                <div className='hidden md:flex items-center '>
-                    <ul className='flex gap-4'>
-                        <li>Home</li>
-                        <li>Our services</li>
-                        <li>About Us</li>
-                    </ul>
-                </div>
-                <div className='hidden md:flex'>
-                    <button className='flex justify-between items-center bg-transparent px-6 gap-2' onClick={handleClick}>
-                        <img src={lock} />
-                        Login
-                    </button>
-                    <Link to="/Styling/Swipe ur syle">
-                        <button className='px-8 py-3 rounded-md bg-[#706fe5] text-white font-bold'>Sign Up For Free</button>
-                    </Link>
-                </div>
-            </div>
-            <div className={toggle ? 'absolute z-10 p-4 bg-white w-full px-8 md:hidden border-b' : 'hidden'}>
-                <ul>
-                    <li className='p-4 hover:bg-black-100'>Home</li>
-                    <li className='p-4 hover:bg-black-100'>Our services</li>
-                    <li className='p-4 hover:bg-black-100'>About Us</li>
-                    <div className='flex flex-col my-4 gap-4'>
-                        <button className='border border-[#706fe5] flex justify-center items-center bg-transparent px-6 gap-2 py-4'>
-                            <img src={lock} />
-                            Login
-                        </button>
-                        <Link to="/Styling/Swipe ur syle">
-                            <button className='px-8 py-5 rounded-md bg-[#706fe5] text-white font-bold'>Sign Up For Free</button>
-                        </Link>
-                    </div>
+  return (
+    <div className='w-full h-[80px] bg-white border-b'>
+        <div className='md:max-w-[1480px] max-w-[600px] m-auto w-full h-full flex justify-between items-center md:px-0 px-4'>
+            
+            <img src={logo} className="h-[200px]" />
+            
+            <div className='hidden md:flex items-center '>
+                <ul className='flex gap-4'>
+                    <li>Home</li>
+                    <li>Our services</li>
+                    <li>About Us</li>
                 </ul>
             </div>
+
+
+            <div className='hidden md:flex'>
+                <button className='flex justify-between items-center  bg-transparent  px-6 gap-2'>
+                    <img src={lock} />
+                    Login
+                </button>
+                <button className='px-8 py-3 rounded-md bg-[#706fe5] text-white font-bold'>Sign Up For Free</button>
+            </div>
+
+            
+
+
+
+
         </div>
-    );
+
+        <div className={toggle?'absolute z-10 p-4  bg-white w-full px-8 md:hidden border-b':'hidden'}>
+            <ul>
+                    <li className='p-4 hover:bg-black-100'>Home</li>
+                    <li className='p-4 hover:bg-black-100'>Our services</li>
+                    <li className='p-4 hover:bg-black-100'>About Ust</li>
+
+                    <div className='flex flex-col my-4 gap-4'>
+                        <button className='border border-[#706fe5] flex justify-center items-center  bg-transparent  px-6 gap-2 py-4'>
+                         <img src={lock} />
+                         Login
+                        </button>
+                        <button className='px-8 py-5 rounded-md bg-[#706fe5] text-white font-bold'>Sign Up For Free</button>
+                    </div>
+            </ul>
+        </div>
+        
+        
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
